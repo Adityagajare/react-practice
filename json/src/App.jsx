91eba60555fb8ef9a8b0afd8c076458d4f8ab1a6
Adityagajare <120468698+Adityagajare@users.noreply.github.com>
2023-03-09 23:08:40 +0530

    Add files via upload

---
+++ json/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import JSON from "./actorsDetails.json";
+import Actors from './Actors';
+import "./global.css"
+
+
+const App = ()=>{
+    return(
+        <div>
+            <Actors data={JSON}/>
+        </div>
+    )
+}
+export default App;

88f12ef3b327dd652777ae94661b2c6dad89a433
Adityagajare <120468698+Adityagajare@users.noreply.github.com>
2023-03-25 21:58:43 +0530

    Add files via upload

---
+++ Signup&Login/src/Login.jsx
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name:'',
+      email:'',
+      error:''
+    }
+  }
+  
+
+
+  handleSubmit = (e) =>{
+    e.preventDefault()
+    let username = localStorage.getItem('username');
+    let password1 = localStorage.getItem('password');
+    console.log(username,password1);
+        if ((username === e.target.name.value) && (password1 === e.target.password.value)) {
+            this.setState({error: 'Successfully Logged In'});
+        } else {
+            this.setState({error: 'Invalid Username or Password'});
+        }
+  }
+ 
+  render() {
+    
+    return (
+      <div>
+        <h1>Login page</h1>
+        <form onSubmit={this.handleSubmit}>
+        <p className="error">
+          {this.state.error}
+        </p>
+        <div>
+        <label htmlFor="username">Username</label>
+                <br />
+                <input type="text" id='username' value={this.state.name} onChange={(e)=>{
+                    this.setState({name:e.target.value})
+                }} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <br />
+                <input type="password" id='password' value={this.state.password} onChange={(e)=>{
+                    this.setState({password:e.target.value})
+                 }} />
+            </div>
+        <button>Login</button>
+      </form>
+      </div>
+    )
+  }
+}
+
+export default Login;
